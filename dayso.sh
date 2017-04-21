a16b196aa05281a214bf2386516e10f6deab9ff5
thanhthuy09 <thanhthuydhcn1a@gmail.com>
2017-04-21 08:23:38 +0700

    bai hoan chinh

--- dayso.sh
+++ dayso.sh
@@ -1,13 +1,16 @@
 echo "Nhap n " 
 read n
-s=0
 for ((i=0; i<n; i++))
 do 
 	echo -n "a[$i]" =
 	read a[$i]
 done
-	echo ${a[*]}
 
-s=$s expr $a[$i]
-echo "tong la : $s"
+echo "day so vua nhap la ${a[*]}"
+
+for ((i=0; i<n; i++))
+do 
+	tong=$(($tong + ${a[$i]}))
+done
+	echo $tong
 
